80670127d83d89a48397d491e3f15575b37ed59e
Jai Dessai <jaistudymail13@gmail.com>
2025-10-05 22:28:06 +0530

    yes this

--- backend/main.py
+++ backend/main.py
@@ -38,4 +38,4 @@ async def health_check():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
